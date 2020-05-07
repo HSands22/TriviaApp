602af683204bf6aab9a96cd93cf237c89ed61c5b
Harry Sands <22sands@friendsseminary.org>
2020-05-07 05:12:37 -0400

    My code

--- Model/QuizBrain.swift
+++ Model/QuizBrain.swift
@@ -24,6 +24,7 @@ struct Question {
     
 }
 
+
 import Foundation
 
 //begin struct
@@ -32,20 +33,63 @@ struct QuizBrain{
 //array of Question structs
 //instead of an array of Strings (like in the Town example) we will have an array of Question structs
 //FORM: let quiz = [Question(q: "question", a: "answer"), Question(q: "questions", a: "answer")]
+    
+    let quiz = [ //begin of array
+    
+        Question(q: "In New York, there are 2.692 million adults who have a mental health condition.", a: "True"),
+        Question(q: "In the U.S., there are 20 million adults who have a mental health condition.", a: "False"),
+        Question(q: "50% of mental illness begins by age 14.", a: "True"),
+        Question(q: "75% of mental illness begins by age 24.", a: "True"),
+        Question(q: "1 American dies by suicide every 12 minutes.", a: "True"),
+        Question(q: "30% of U.S. adults with mental illness also have a substance use disorder.", a: "False"),
+        Question(q: "70% of people experiencing homelessness also have a serious mental illness.", a: "False"),
+        Question(q: "37% of people incarcerated in state and federal prison have a diagnosed mental condition.", a: "True"),
+        Question(q: "60% of youth in the juvenile justice system have at least one mental health condition.", a: "False"),
+        Question(q: "Depression and anxiety disorders cost the global economy $1,000,000,000,000 each year in lost productivity.", a: "True"),
+        Question(q: "1 in 15 Americans has a mental health condition.", a: "False"),
+        Question(q: "8 million adults live with a severe mental illness.", a: "False"),
+        Question(q: "1 out of every 8 emergency department visits involves a mental health or substance use condition.", a: "True"),
+        Question(q: "Depression is the #1 leading cause of disability in the world.", a: "True"),
+        Question(q: "16% of people who die by suicide have a diagnosed mental health condition.", a: "False"),
+        Question(q: "90% of people who die by suicide have experienced symtpoms of a mental health condition.", a: "True"),
+        Question(q: "1 million people in New York live in a mental health professional shortage area.", a: "False"),
+        Question(q: "50 million people in the U.S. live in a mental health professional shortage area.", a: "False"),
+        Question(q: "The average delay between mental health symptom onset and treatment is 11 years.", a: "True"),
+        Question(q: "About 4 in 10 people with a mental health condition received any treatment in a year.", a: "True"),
+        ]
    
     
 //variable for our array index - Int
+    var arrayIndex : Int = 0
 //variable for number of right answers - Int
+    var scoreRight : Int = 0
 //variable for number of wrong answers - Int
+    var scoreWrong : Int = 0
 //variable to act as a flag if the user reaches the end of the quiz - Int or Boolean or String
+    var endOfQuiz : String = "F"
 
 //FUNCTION - checking the users answer. This will check if the String sent from the ViewController (ie, the user's answer) matches the String from the array of Question structs (ie, the answer)
     //FORM: func function_name(user's answer (ie a String) -> returns a true or false
     //conditional statement that will return true if user's answer matches, false if not
+    func checkAnswer(answer : String) -> Bool{
+        
+        if (answer == quiz[arrayIndex].answer){
+            return true
+        }
+        else{
+            return false
+        }
+   }
 
 //FUNCTION - retrieving the questions text from the array of Question structs
     //FORM: func functions_name() -> returns question (ie String)
-
+    func showQuestion() -> String{
+        
+        return quiz[arrayIndex].text
+        
+    }
+       
+    
 //FUNCTION - Retrieves user's progress (question number / total number of questions)
     //FORM: func functions_name() -> returns a number (ie Float)
 
@@ -54,5 +98,11 @@ struct QuizBrain{
 
 //FUNCTION - updates the variable that is acting as our array index (we need a "pointer" that will keep track of where in the array we are). Useful for knowing what question we're on and when the quiz is over.
     //FORM: func function_name()
+    mutating func moveForward() {
+        
+        if(arrayIndex < 19) {
+            arrayIndex = arrayIndex + 1
+        }
+    }
 
 }
--- TriviaApp/ViewController.swift
+++ TriviaApp/ViewController.swift
@@ -8,45 +8,110 @@
 
 import UIKit
 
+//information for questions from Nami's website
 class ViewController: UIViewController {
-
+    var quizBrain = QuizBrain()
+    
+    var timer = Timer()
+    
+    //number of total seconds
+    var totalTime: Float = 5
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // THIS FUNCTION ALWAYS RUNS WHEN THE APP LOADS
         //VERY SIMILIAR TO SETUP() IN PROCESSING
         //THIS IS WHERE WE WILL CALL/ACTIVATE OUR CUSTOM FUNCTION, UPDATEUI()
         //UPDATEUI WILL SETUP EVERYTHING FOR THE REST OF THE CODE TO WORK
+        currentQuestion.text = quizBrain.showQuestion()
+      
+        rightAnswer.alpha = 0
+        wrongAnswer.alpha = 0
+        
+        endMessage.alpha = 0
     }
     
     //UPDATEUI WILL UPDATE ANY ON SCREEN ELEMENTS AS WELL AS SETUP EVERYTHING FOR OUR FIRST PLAY THROUGH
     @objc func updateUI(){
         
+        
+        
         //CLEAR ANY COLOR CHANGES (FOR INSTANCE, IF THE COLOR CHANGES WHEN USER GETS QUESTION RIGHT/WRONG, WE WANT THE COLOR TO CHANGE BACK AFTER THEY ANSWER THE QUESTION
+        rightAnswer.alpha = 0
+        wrongAnswer.alpha = 0
         
         //UPDATE THE PROGRESS BAR
         
         //UPDATE THE QUESTION THAT APPEARS ON SCREEN
+        quizBrain.moveForward()
+        currentQuestion.text = quizBrain.showQuestion()
+        
         
         //CONDITIONAL THAT WILL CHECK IF THE QUIZ IS OVER. SOME KIND ON SCREEN FEEDBACK SHOULD HAPPEN AT THIS POINT. DISPLAY SCORE, CONGRATULATE THE PLAYER, ETC
+        if(quizBrain.arrayIndex == 19) {
+            print("DONE")
+            
+            endMessage.alpha = 1.0
+            currentQuestion.alpha = 0
+            rightAnswer.alpha = 0
+            wrongAnswer.alpha = 0
+        }
         
         
     }
     
+    @IBOutlet weak var rightAnswer: UILabel!
+    
+    @IBOutlet weak var wrongAnswer: UILabel!
+    
+    @IBOutlet weak var endMessage: UILabel!
+   
+    @IBOutlet weak var currentQuestion: UILabel!
+    
     //IBACTION FUNCTION (TRUE AND FALSE BUTTONS SHOULD BE LINKED TO THIS FUNCTION)
     
+    @IBAction func answerSelected(_ sender: UIButton) {
+        
+    
             //STORE THE CURRENTTITLE OF THE BUTTON IN A VARIABLE
+        var currentTitle = sender.currentTitle!
     
             //CHECK THE ANSWER USING QUIZBRAIN
-    
+        if(quizBrain.checkAnswer(answer: currentTitle) == true) {
+            //change color
+            sender.tintColor = UIColor.green
+            print("R")
+            rightAnswer.alpha = 1.0
+        }
+        else{
+            //change color
+            sender.tintColor = UIColor.red
+            print("W")
+            wrongAnswer.alpha = 1.0
+        }
+        
             //CONDITIONAL STATEMENT THAT WILL:
                 //CHANGE THE BUTTONS COLOR DEPENDING IF THEY GOT IT RIGHT OR WRONG
                 //UPDATE THE SCORE VARIABLES
                 //INFORM THE USER IF THEY GOT IT RIGHT OR WRONG
             
             //USE A TIMER TO CALL UPDATEUI() TO CHANGE ON SCREEN ELEMENTS (UPDATE QUESTION TEXT, PROGRESS, SCORE, ETC). ITS A GOOD IDEA TO USE A TIMER SO WE CAN CONTROL HOW LONG THE COLOR CHANGES. EXAMPLE: USER PRESSES THE BUTTON->BUTTON COLOR CHANGES->TIMER FOR # OF SECONDS->CALL UPDATEUI() USING TIMER. REVIEW MUSICAPP FOR MORE REVIEW
+        
+        
+        //timer = timer + 1
+        
+       // timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.timerAction), userInfo: nil, repeats: true)
+        
+    }
     
-            //GO TO THE NEXT QUESTION
-
+    @IBAction func nextQuestion(_ sender: UIButton) {
+        
+        //GO TO THE NEXT QUESTION
+            updateUI()
+        
+    }
+    
 
 }
 
